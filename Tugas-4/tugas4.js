1c48234e9707d096d8cef47b0d01d56e34eb23e9
Falia <faliavicky1128@gmail.com>
2020-10-02 05:43:32 +0700

    update tugas-3

--- Tugas-4/tugas4.js
+++ Tugas-4/tugas4.js
@@ -1,4 +1,5 @@
 // soal 1
+//jawaban soal 1
 var loop = ['looping pertama','loopping kedua'];
 
 console.log(loop[0].toUpperCase());
@@ -18,6 +19,7 @@ while(kedua>=2){
 
 
 //soal 2
+//jawaban soal 2
 for(angka=1;angka<=20;angka++){
     if(angka%2==0){
         console.log(angka + ' ' + '- Berkualitas');
@@ -32,7 +34,7 @@ for(angka=1;angka<=20;angka++){
 
 
 //soal 3
-var a = '#';
+//jawaban soal 3
 for (i=1 ; i<=7 ; i++){
     var c =' ';
     for (j=1 ; j<=i ; j++){
@@ -45,13 +47,14 @@ for (i=1 ; i<=7 ; i++){
 
 //soal 4
 var kalimat="saya sangat senang belajar javascript";
+//jawaban soal 4
 var pisah= kalimat.split(" ");
 console.log(pisah);
 
 
 //soal 5
 var daftarBuah = ["2. Apel", "5. Jeruk", "3. Anggur", "4. Semangka", "1. Mangga"];
-var daftarBuah = ["2. Apel", "5. Jeruk", "3. Anggur", "4. Semangka", "1. Mangga"];
+//jawaban soal 5
 daftarBuah.sort();
 for(i=0; i<=4; i++){
     console.log(daftarBuah[i]);
